2788cc14973a0eff18662163edc98072b89812eb
smalldog124 <panumars124@gmail.com>
2020-04-23 11:59:52 +0700

    [Added]: เพิ่มฟังก์ชันของ NewBook

--- books/books.go
+++ books/books.go
@@ -1,8 +1,19 @@
 package books
 
+import (
+	"math/rand"
+)
+
 type Books struct {
 	ID    int
 	Name  string
-	Price float64
+	Price int
 	Stock int
+}
+
+func (b *Books) NewBook(name string, price int) {
+	b.ID = rand.Int()
+	b.Name = name
+	b.Price = price
+	b.Stock = 10
 }
